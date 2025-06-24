c4dc950d0226d3067aec7c74670e5106a6d2ca02
Jalu Fazlurrahman <77714640+0zyyy@users.noreply.github.com>
2025-06-25 02:37:41 +0700

    feat: repl read from a file instead of prompting

--- src/monkey/main.go
+++ src/monkey/main.go
@@ -8,12 +8,18 @@ import (
 )
 
 func main() {
-	user, err := user.Current()
+	_, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
-		user.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+
+	// Read the monkey program from file
+	content, err := os.ReadFile("main.monk")
+	if err != nil {
+		fmt.Printf("Error reading file: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Executing program from main.monk\n\n\n")
+	// Pass the file content directly to REPL
+	repl.Start(string(content), os.Stdout)
 }
